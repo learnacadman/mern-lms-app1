edbf743b327c78d7017980ec19fee4ac859f96a8
learnacadman <105061140+learnacadman@users.noreply.github.com>
2023-11-24 13:09:22 +0530

    Add files via upload

--- Front-end/src/App.js
+++ Front-end/src/App.js
@@ -1,78 +1,81 @@
-import React, {Component} from 'react';
-import {Route, Switch, Redirect} from 'react-router-dom';
-import {BrowserRouter} from 'react-router-dom';
-import Login from './Pages/Auth/Forms/Login/Login';
-import Signup from './Pages/Auth/Forms/Signup/Signup';
-import EmailVerify from './Pages/Auth/Forms/ForgotPassword/EmailVerify';
-import ForgotPasswordotp from './Pages/Auth/Forms/ForgotPassword/ForgotPassOtp';
-import ResetPassword from './Pages/Auth/Forms/ResetPassword/ResetPassword';
-import Cart from './Pages/Cart/Cart'
-import Otp from './Pages/Auth/Forms/Otp/Otp';
-import Homepage from './Pages/HomePage/Homepage';
-import TeacherPage from './Pages/Teacher/TeacherPage';
-import TeacherVideos from './Pages/Teacher/TeacherVideos';
-import TeacherHomePage from './Pages/Teacher/TeacherHomepage/TeacherHomepage';
-import TeacherEdit from './Pages/Teacher/TeacherHomepage/TeacherEdit';
-import CoursePage from './Pages/CoursePage/CoursePage';
-import Preference from './Pages/HomePage/Preference';
-import Stripe from './Pages/payment/StripeContainer';
-import Chat from './Pages/Chat/Chat';
-
+import React, { Component } from "react";
+import { Route, Routes, Navigate } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+import Login from "./Pages/Auth/Forms/Login/Login";
+import Signup from "./Pages/Auth/Forms/Signup/Signup";
+import EmailVerify from "./Pages/Auth/Forms/ForgotPassword/EmailVerify";
+import ForgotPasswordotp from "./Pages/Auth/Forms/ForgotPassword/ForgotPassOtp";
+import ResetPassword from "./Pages/Auth/Forms/ResetPassword/ResetPassword";
+import Cart from "./Pages/Cart/Cart";
+import Otp from "./Pages/Auth/Forms/Otp/Otp";
+import Homepage from "./Pages/HomePage/Homepage";
+import TeacherPage from "./Pages/Teacher/TeacherPage";
+import TeacherVideos from "./Pages/Teacher/TeacherVideos";
+import TeacherHomePage from "./Pages/Teacher/TeacherHomepage/TeacherHomepage";
+import TeacherEdit from "./Pages/Teacher/TeacherHomepage/TeacherEdit";
+import CoursePage from "./Pages/CoursePage/CoursePage";
+import Preference from "./Pages/HomePage/Preference";
+import Stripe from "./Pages/payment/StripeContainer";
 
 class App extends Component {
-
-
-
-  render(){
-
-
-
+  render() {
     return (
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" exact component={Signup} />
+          <Route path="/login" component={Login} />
+          <Route path="/signup/otp" component={Otp} />
+          <Route path="/forgotpasswordemail" component={EmailVerify} />
+          <Route path="/ForgotPasswordotp" component={ForgotPasswordotp} />
+          <Route path="/ResetPassword" component={ResetPassword} />
 
-      <BrowserRouter >
-    
-        <Switch>
+          <Route
+            path="/home/:CourseName"
+            exact
+            render={(props) => (
+              <Homepage key={props.location.pathname} {...props} />
+            )}
+          />
+          <Route
+            path="/home/Interest/Preference"
+            exact
+            component={Preference}
+          />
 
-        <Route path="/signup" exact component={Signup}/>
-        <Route path="/login" component={Login}/>
-        <Route path="/signup/otp"  component={Otp}/>
-        <Route path="/forgotpasswordemail"  component={ EmailVerify}/>
-        <Route path='/ForgotPasswordotp' component={ForgotPasswordotp}/>
-        <Route path="/ResetPassword" component= {ResetPassword}/>
+          <Route
+            path="/course/:Course/:Courseid"
+            exact
+            render={(props) => (
+              <CoursePage key={props.location.pathname} {...props} />
+            )}
+          />
 
-        <Route path="/home/:CourseName" exact    render={props =>
-        <Homepage key={props.location.pathname} {...props}/>}/>
-        <Route path="/home/Interest/Preference" exact component={Preference}/>
+          <Route path="/Cart" component={Cart} />
 
-          <Route path="/course/:Course/:Courseid" exact    render={props =>
-        <CoursePage key={props.location.pathname} {...props}/>}/>
+          <Route
+            path="/stripe/:CourseLink"
+            exact
+            render={(props) => (
+              <Stripe key={props.location.pathname} {...props} />
+            )}
+          />
 
+          <Route path="/Teacher" component={TeacherPage} />
+          <Route
+            path="/TeacherVideos"
+            render={(props) => <TeacherVideos {...props} />}
+          />
 
-        <Route path="/Cart" component={Cart}/>
-        
-        <Route path="/stripe/:CourseLink" exact render={props=>
-        <Stripe key={props.location.pathname} {...props} />}/>
+          <Route path="/TeacherHome" component={TeacherHomePage} />
+          <Route path="/TeacherEdit" component={TeacherEdit} />
 
-        <Route path="/Teacher" component={TeacherPage}/>
-          <Route path="/TeacherVideos" 
-          render={(props)=> <TeacherVideos {...props}/> }/>
+          {/* chat room for teacher and student */}
 
-        <Route path="/TeacherHome" component={TeacherHomePage}/>
-          <Route path="/TeacherEdit" component={TeacherEdit}/>
-
-        {/* chat room for teacher and student */}
-
-        <Route path="/chat" component={Chat}/>
-
-        <Redirect to="/home/all"/>
-
-
-       </Switch>
-
-  </BrowserRouter>
-
-
-  );
-}}
+          <Navigate to="/home/all" />
+        </Routes>
+      </BrowserRouter>
+    );
+  }
+}
 
 export default App;
